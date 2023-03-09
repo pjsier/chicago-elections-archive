5e6153267ecca48b26bc05a105029b65b6a13c02
pjsier <pjsier@gmail.com>
2023-03-08 18:18:07 -0600

    chore: update updated time

--- src/js/pages/map-page.js
+++ src/js/pages/map-page.js
@@ -135,7 +135,7 @@ const MapPage = (props) => {
           />
           <Show when={UNOFFICIAL_RESULTS.includes(state.election)}>
             <p class="unofficial-notice">
-              Unofficial results as of Mar. 6, 5:11pm
+              Unofficial results as of Mar. 8, 3:40pm
             </p>
           </Show>
           <Show when={props.embedElection}>
